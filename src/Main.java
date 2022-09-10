a4373a6d24bc6b3bb2f5b8cc21fc13d6f45c8b17
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-10 23:19:53 +0400

    Задача 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задача 1.");
+        int i = 7;
+        byte b = 120;
+        short s = -1000;
+        long l = 19797457;
+        float f = 64.46f;
+        double d = 365.86;
+        System.out.println("Значение переменной 'i' с типом 'int' равно: " + i);
+        System.out.println("Значение переменной 'b' с типом 'byte' равно: " + b);
+        System.out.println("Значение переменной 's' с типом 'short' равно: " + s);
+        System.out.println("Значение переменной 'l' с типом 'long' равно: " + l);
+        System.out.println("Значение переменной 'f' с типом 'float' равно: " + f);
+        System.out.println("Значение переменной 'd' с типом 'double' равно: " + d);
     }
 }
