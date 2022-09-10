da2f90dab4cce2b8f41cce4cae834f3dcfc4b92e
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-11 00:14:18 +0400

    Задача 3.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 2.");
-        int i = 27897;
-        byte by = 67;
-        short s = -159;
-        long l = 987_678_965_549L;
-        float f = 27.12F;
-        double d = 2.786;
-        boolean b = false;
-        char c = 569;
+        System.out.println("Задача 3.");
+        int FirstGradeTeacher = 23;
+        int SecondGradeTeacher = 27;
+        int ThirdGradeTeacher = 30;
+        int NumberSheetsPaper= 480;
+        int NumberSheetsChildren = NumberSheetsPaper / (FirstGradeTeacher + SecondGradeTeacher + ThirdGradeTeacher);
+        System.out.println("На каждого ученика рассчитано " + NumberSheetsChildren + " листов бумаги.");
     }
 }
