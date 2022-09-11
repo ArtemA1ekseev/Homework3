2751588abbbcc73d9bf1097bd25add707700e93d
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-11 12:38:52 +0400

    Задача 6.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 5.");
-        int WholeCansPaints = 120;
-        int WhiteCansPaints = 2;
-        int BrownCansPaints = 4;
-        int NumberClasses = WholeCansPaints / (WhiteCansPaints + BrownCansPaints);
-        int WhiteCansPaintsAll = NumberClasses * WhiteCansPaints;
-        int BrownCansPaintsAll = NumberClasses * BrownCansPaints;
-        System.out.println("В школе, где " + NumberClasses + " классов, нужно " + WhiteCansPaintsAll + " банок белой краски и " + BrownCansPaintsAll + " банок коричневой краски.");
+        System.out.println("Задача 6.");
+        int WeightOneBananas = 80;
+        int QuantityBananas = 5;
+        int GramProductBananas = QuantityBananas * WeightOneBananas;
+        int WeightOneMilk = 105;
+        int QuantityMilk = 200;
+        int GramProductMilk = WeightOneMilk * (QuantityMilk / 100);
+        int WeightOneIceCream = 100;
+        int QuantityIceCream = 2;
+        int GramProductIceCream = QuantityIceCream * WeightOneIceCream;
+        int WeightOneEggs = 70;
+        int QuantityEggs = 4;
+        int GramProductEggs = QuantityEggs * WeightOneEggs;
+        int FinalWeightProduct = GramProductBananas + GramProductMilk + GramProductIceCream + GramProductEggs;
+        int grPerKg = 1000;
+        float weightKg = FinalWeightProduct/(float)grPerKg;
+        System.out.println("Общий вес спорт-завтрака - " + weightKg + " кг.");
     }
 }
